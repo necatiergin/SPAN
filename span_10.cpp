62803127fcc9976c03c3cc1e5aead781b9cb3d7c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-21 10:59:07 +0300

    Update span_10.cpp

--- span_10.cpp
+++ span_10.cpp
@@ -2,7 +2,6 @@
 #include <span>
 #include <iostream>
 
-
 std::vector<int> get_vec()
 {
 	return { 1, 2, 3, 4, 5, 6, 7 };
@@ -10,7 +9,7 @@ std::vector<int> get_vec()
 
 int main()
 {
-	std::span x{ get_vec().begin(), 5}; //ub
+	std::span x{ get_vec().begin(), 5}; //ub - dangling pointer
 
 	for (auto i : x)
 		std::cout << i << " ";
