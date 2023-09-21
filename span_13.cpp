01a6c3b5238746ecb077b658e7bcc2492f5f0dee
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-21 11:55:15 +0300

    Update span_13.cpp

--- span_13.cpp
+++ span_13.cpp
@@ -1,24 +1,27 @@
-#include <iostream>
 #include <span>
+#include <vector>
+#include <iostream>
 
-template<typename T, std::size_t length>
-void print(std::span<T, length> span) {
-    for (const auto &elem : span) {
-        std::cout << elem << ' ';
-    }
-    std::cout << '\n';
-}
-
-int main() 
+int main()
 {
-    int ar[] = { 1, 2, 3, 4, 5 };
-    
-    //print(ar); //error
-    print(std::span{ ar});
-    print(std::span{ ar, 1});
-    print(std::span{ ar, 2});
-    print(std::span{ ar, 3});
-    print(std::span{ ar, 4});
-    print(std::span{ ar, 5});
-    print(std::span{ ar + 1, 3});
+    std::vector v = { 1, 2, 3, 4, 5 };
+    std::span sp{ v };
+
+    std::cout << "sp.size() = " << sp.size() << '\n';
+    std::cout << "sp.size_bytes() = " << sp.size_bytes() << '\n';
+    std::cout << "sp.extent = " << sp.extent << '\n';
+
+    std::cout << sp[0] << '\n';
+    sp[0] *= 10;
+    std::cout << v[0] << '\n';
+    std::cout << sp.front() << '\n';
+    ++sp.front();
+    std::cout << v[0] << '\n';
+    sp.back() *= 10;
+    std::cout << v.back() << '\n';
+
+    boolalpha(std::cout);
+    std::cout << "sp.empty() = " << sp.empty() << '\n';
+    for (size_t i{}; i < sp.size(); ++i)
+        std::cout << *(sp.data() + i) << ' ';    
 }
