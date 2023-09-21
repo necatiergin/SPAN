aa55f512a904375269c0a475bd0be8fd6c22dee4
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-21 17:14:15 +0300

    Update span_01.cpp

--- span_01.cpp
+++ span_01.cpp
@@ -1,1 +1,13 @@
+#include <span>
+#include <iostream>
 
+int main()
+{
+	int a[10]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	std::span<int, 10> sp1{ a };
+	std::span<int> sp2{ a };
+
+	std::cout << "sizeof(sp1) = " << sizeof(sp1) << "\n";
+	std::cout << "sizeof(sp2) = " << sizeof(sp2) << "\n";
+}
