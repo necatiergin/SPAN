352d97e208c78324ab58522780dbabf9fae56e7e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-21 12:31:27 +0300

    Create span_14.cpp

---
+++ span_14.cpp
@@ -0,0 +1,18 @@
+#include <span>
+#include <ranges>
+#include <iostream>
+
+//std::span is a view
+
+int main()
+{
+	namespace rng = std::ranges;
+	namespace vw = std::views;
+	
+	std::span sp{ "necati ergin" };
+
+	static_assert(rng::view<decltype(sp)>); //holds
+
+	for (auto c : sp | vw::drop(7) | vw::take(3))
+		std::cout.put(c);
+}
