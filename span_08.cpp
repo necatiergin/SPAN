792b47c3ff3c42bd7eb8b4bbd887bc1892fbb096
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-21 10:56:00 +0300

    Update span_08.cpp

--- span_08.cpp
+++ span_08.cpp
@@ -6,10 +6,10 @@ int main()
 	std::array a1{ 1, 2, 3, 4, 5, 6,7 ,8, 9, 10 };
 	std::array a2{ 0, 8, 15 };
 	const std::span<int> cspn{ a1 }; // span itself is const
-	std::span<const int> spc{ a1 }; // viewed elements are const
-	cspn[0] = 42; //gecerli
-	++cspn.back(); //gecerli
-	//spc[0] = 42; // gecersiz
-	//cspn = a2; // gecersiz cpsn const
-	spc = a2; // gecerli
+	std::span<const int> sp{ a1 }; // viewed elements are const
+	cspn[0] = 42; // valid
+	++cspn.back(); // valid
+	//sp[0] = 42; // error
+	//cspn = a2; // error cpsn const
+	sp = a2; // valid
 }
