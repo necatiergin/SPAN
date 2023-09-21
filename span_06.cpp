46c4c08f99e5ff27bbc0c3eca24befd23fa28cdc
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-21 10:53:04 +0300

    Update span_06.cpp

--- span_06.cpp
+++ span_06.cpp
@@ -5,7 +5,7 @@ int main()
 {
 	std::vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 	std::span<const int> sp1{ ivec }; //ok 
-	//std::span<long long> sp2{ ivec }; // gecersiz
-	//std::span<int> sp3{ sp1 }; // gecersiz
-	//std::span<const long> sp4{ sp1}; // gecersiz
+	//std::span<long long> sp2{ ivec }; // error
+	//std::span<int> sp3{ sp1 }; // error
+	//std::span<const long> sp4{ sp1}; // error
 }
