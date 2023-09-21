b73534ad41c2df5c524690707fc6b56bea415096
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-09-21 18:48:42 +0300

    Create span_20.cpp

---
+++ span_20.cpp
@@ -0,0 +1,13 @@
+#include <span>
+#include <vector>
+#include <ranges>
+
+int main()
+{
+	using namespace std;
+
+	vector ivec{ 2, 5, 7, 9, 3, 6 };
+
+	auto spn = views::counted(ivec.begin(), 5);
+	static_assert(same_as<decltype(spn), span<int>>); //holds
+}
