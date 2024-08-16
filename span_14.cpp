638f4615347a8b477d1b2c858ffc5ee380e3746d
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-16 18:02:20 +0300

    Update span_14.cpp

--- span_14.cpp
+++ span_14.cpp
@@ -2,15 +2,17 @@
 #include <ranges>
 #include <iostream>
 
+//std::span is a range
 //std::span is a view
 
 int main()
 {
 	namespace rng = std::ranges;
 	namespace vw = std::views;
-	
+
 	std::span sp{ "necati ergin" };
 
+	static_assert(rng::range<decltype(sp)>); //holds
 	static_assert(rng::view<decltype(sp)>); //holds
 
 	for (auto c : sp | vw::drop(7) | vw::take(3))
