47a0b686ef066148ec4cdfed90801865dcc6ccc6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-16 15:51:38 +0300

    Update span_02.cpp

--- span_02.cpp
+++ span_02.cpp
@@ -18,7 +18,7 @@ int main()
                 std::cout << "static extent = " << ex << '\n';
         };
 
-    int a[]{1, 2, 3, 4, 5};
+    int a[]{ 1, 2, 3, 4, 5 };
 
     std::span span1{ a };
     print("span1", span1.extent);
@@ -26,11 +26,11 @@ int main()
     std::span<int, std::dynamic_extent> span2{ a };
     print("span2", span2.extent);
 
-    std::array ar{1, 2, 3, 4, 5};
+    std::array ar{ 1, 2, 3, 4, 5 };
     std::span span3{ ar };
     print("span3", span3.extent);
 
-    std::vector v{1, 2, 3, 4, 5} ;
+    std::vector v{ 1, 2, 3, 4, 5 };
     std::span span4{ v };
     print("span4", span4.extent);
 }
