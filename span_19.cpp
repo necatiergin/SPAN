16e3e4b709afaea6d7e2a25d4a1b4e799833259b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-17 10:44:35 +0300

    Update span_19.cpp

--- span_19.cpp
+++ span_19.cpp
@@ -8,6 +8,7 @@ int main()
 
 	vector ivec{ 2, 5, 7, 9, 3, 6 };
 
+	//some adaptors return spans
 	auto spn = views::counted(ivec.begin(), 5);
 	static_assert(same_as<decltype(spn), span<int>>); //holds
 }
