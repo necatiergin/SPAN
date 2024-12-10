6c6a52f3899a883582e615d5a97d1ddc760df171
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-10 12:34:21 +0300

    Update span_05.cpp

--- span_05.cpp
+++ span_05.cpp
@@ -3,11 +3,11 @@
 
 int main()
 {
-	int c_ar[10];
+	int c_ar[10]{};
 	std::array ar{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	std::span<int> sp1{ c_ar}; // ok
+	std::span<int> sp1{ c_ar }; // ok
 	std::span<int> sp2{ ar }; // ok
-	//std::span<int, 5> sp3{ ar}; // gecersiz
-	//std::span<int, 5> sp4{c_ar}; // gecersiz
+	//std::span<int, 5> sp3{ ar}; // invalid
+	//std::span<int, 5> sp4{c_ar}; // invalid
 	std::span<int, 5> sp5{ ar.data(), 5 }; // ok
 }
